b651bd9dc0c1ef7491feeeebe782c0bca854cbc4
ChoEun-Sang <wh94es@gmail.com>
2024-07-25 19:34:38 +0900

    chore: rollup-plugin-visualizer 설치

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,27 @@
-import { defineConfig } from "vite";
+import { defineConfig, type PluginOption } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      plugins: [
+        visualizer({
+          filename: "./dist/report.html",
+          open: true,
+          gzipSize: true,
+          brotliSize: true
+        }) as PluginOption
+      ]
+    }
+  },
   base: "/sweethome",
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "./src")
     }
-  },
-  
+  }
 });
