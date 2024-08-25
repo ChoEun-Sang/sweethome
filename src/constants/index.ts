d958dbacba846a9d95f70d390063b80cafbcdfdd
ChoEun-Sang <wh94es@gmail.com>
2024-08-25 15:52:45 +0900

    refactor: constants 폴더 이동 및 상품 필터 const 추가

--- src/constants/index.ts
+++
@@ -1,6 +0,0 @@
-export const SELECT_TAGS = [
-  {name: '태그를 선택해주세요.', value: ''},
-  {name: 'FURNITURE', value: 'FURNITURE'},
-  {name: 'KITCHEN', value: 'KITCHEN'},
-  {name: 'BEDROOM', value: 'BEDROOM'}
-]
